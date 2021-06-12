937bf1f90270d57383b67afe800be31408e7119f
spotsccc <chernenko.ivan.k17@gmail.com>
2021-06-12 22:36:32 +0300

    kek

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ const button = document.querySelector('button');
 const h = document.querySelector('h1');
 
 button.addEventListener('click', () => {
-    if (count % 5 === 0) {
+    if (count % 5 === 0 && count !== 0) {
         alert('Да ты заебал!')
     }
     h.textContent = `Count: ${count++}`;
