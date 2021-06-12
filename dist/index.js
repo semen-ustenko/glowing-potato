d055454aac0e6a01d09bb5bd06c60f23cf90ac2f
spotsccc <chernenko.ivan.k17@gmail.com>
2021-06-12 22:32:25 +0300

    kek

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@ let count = 0;
 const button = document.querySelector('button');
 const h = document.querySelector('h1');
 
-button.addEventListener('click', () => {
+button.addEventListener('click', () => { 
     h.textContent = `Count: ${count++}`;
 });
