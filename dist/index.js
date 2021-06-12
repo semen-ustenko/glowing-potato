5c82deabe9ad8429c5812b3835c5e1c451d6191c
spotsccc <chernenko.ivan.k17@gmail.com>
2021-06-12 22:56:07 +0300

    kek

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,6 @@ const button = document.querySelector('button');
 const input = document.querySelector('input');
 
 button.addEventListener('click', () => {
+    console.log(234)
     alert(input.value);
 })
