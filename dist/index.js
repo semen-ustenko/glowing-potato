8bea8eb3ed424c5a3cddb85e4b7cc0333ae2c46e
spotsccc <chernenko.ivan.k17@gmail.com>
2021-06-14 22:36:41 +0300

    hello world

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-
-button.addEventListener('click', () => {
-    console.log(234)
-    alert(input.value);
-})
